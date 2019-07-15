194173760ec735d0247dcfa2ab93336e1aa8ce66
Teng YANG <teng.yang@oocl.com>
2019-07-15 17:45:53 +0800

    refactor: Extra constants in price calculator

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -1,16 +1,21 @@
 package com.tws.refactoring.extract_variable;
 
 public class PriceCalculator {
+    private static final int MAX_DISCOUNT = 500;
+    private static final int MAX_SHIPPING_COST = 100;
+    private static final double DISCOUNT_RATE = 0.05;
+    private static final double SHIPPING_RATE = 0.1;
+
     double getPrice(int quantity, int itemPrice) {
         // Price consists of: base price - discount + shipping cost
         return quantity * itemPrice - getDiscount(quantity, itemPrice) + getShippingCost(quantity, itemPrice);
     }
 
     double getDiscount(int quantity, int itemPrice) {
-        return Math.max(0, quantity - 500) * itemPrice * 0.05;
+        return Math.max(0, quantity - MAX_DISCOUNT) * itemPrice * DISCOUNT_RATE;
     }
 
     double getShippingCost(int quantity, int itemPrice) {
-        return Math.min(quantity * itemPrice * 0.1, 100);
+        return Math.min(quantity * itemPrice * SHIPPING_RATE, MAX_SHIPPING_COST);
     }
 }

93d1f8e4d61641e4dcfde8bf1227ead014fd0076
Teng YANG <teng.yang@oocl.com>
2019-07-15 17:43:18 +0800

    refactor: Split logic in price calculator

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -3,8 +3,14 @@ package com.tws.refactoring.extract_variable;
 public class PriceCalculator {
     double getPrice(int quantity, int itemPrice) {
         // Price consists of: base price - discount + shipping cost
-        return quantity * itemPrice -
-                Math.max(0, quantity - 500) * itemPrice * 0.05 +
-                Math.min(quantity * itemPrice * 0.1, 100.0);
+        return quantity * itemPrice - getDiscount(quantity, itemPrice) + getShippingCost(quantity, itemPrice);
+    }
+
+    double getDiscount(int quantity, int itemPrice) {
+        return Math.max(0, quantity - 500) * itemPrice * 0.05;
+    }
+
+    double getShippingCost(int quantity, int itemPrice) {
+        return Math.min(quantity * itemPrice * 0.1, 100);
     }
 }
